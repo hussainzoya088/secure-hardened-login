3cdb52aaa51d588fcbccee7bc7615bd7583d17aa
YourGitHubUsername <your-github-email@example.com>
2026-02-12 14:53:44 +0530

    Add: Configuration template for easier setup

---
+++ app/config.sample.php
@@ -0,0 +1,9 @@
+<?php
+
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'secure_login_db');
+define('DB_USER', 'root');
+define('DB_PASS', ''); // Enter your password here
+define('SITE_KEY', 'your_random_key_here');
+?>
